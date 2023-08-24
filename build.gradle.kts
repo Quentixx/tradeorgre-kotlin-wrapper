bee8503eada63e80375e7b81aacd4a0782f79bce
Quentin <nelsonsims@hotmail.fr>
2023-08-24 18:40:12 +0200

    chore(clean): Cleaning code

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,8 @@ repositories {
     mavenCentral()
     gradlePluginPortal()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    maven {
-        url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
-    }
-    maven {
-        url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-    }
+    maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
 
 dependencies {
